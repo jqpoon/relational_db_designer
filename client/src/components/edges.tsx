cbbfc8bf9e76f0a0d3cb1e658aa62e2f823beb0d
Jia Qi Poon <poonjiaqi@gmail.com>
2021-10-20 17:58:16 +0100

    Added basic custom edge

--- client/src/components/edges.tsx
+++ client/src/components/edges.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
-import { getBezierPath, getMarkerEnd } from 'react-flow-renderer';
+import { ArrowHeadType, getBezierPath, getMarkerEnd , Position, getEdgeCenter} from 'react-flow-renderer';
+import './edges.css'
 
 export interface EdgeProps {
-  id: number;
+  id: string;
   sourceX: number,
   sourceY: number,
   targetX: number,
   targetY: number,
-  sourcePosition: number,
-  targetPosition: number,
+  sourcePosition: Position,
+  targetPosition: Position,
   style: any, // Used to be {}
   data: any,
-  arrowHeadType: any,
+  arrowHeadType: ArrowHeadType,
   markerEndId: any,
 }
 
-// export default function CustomEdge(props : EdgeProps) {
-//   let sourceX = props.id;
-//   const edgePath = getBezierPath({ props.sourceX, sourceY, sourcePosition, targetX, targetY, targetPosition });
-//   const markerEnd = getMarkerEnd(arrowHeadType, markerEndId);
+const foreignObjectSize = 200;
 
-//   return (
-//     <>
-//       <path id={id} style={style} className="react-flow__edge-path" d={edgePath} markerEnd={markerEnd} />
-//       <text>
-//         <textPath href={`#${id}`} style={{ fontSize: '12px' }} startOffset="50%" textAnchor="middle">
-//           {data.text}
-//         </textPath>
-//       </text>
-//     </>
-//   );
+const onEdgeClick = (evt: any, id: any) => {
+  evt.stopPropagation();
+  alert(`remove ${id}`);
+};
+
+export default function AttributeEdge(props : EdgeProps) {
+  let edgePathDict = {sourceX: props.sourceX, sourceY: props.sourceY, sourcePosition: props.sourcePosition, targetX: props.targetX, targetY: props.targetY, targetPosition: props.targetPosition};
+  const edgePath = getBezierPath(edgePathDict);
+  const markerEnd = getMarkerEnd(props.arrowHeadType, props.markerEndId);
+
+  let randomDict = {
+    sourceX: props.sourceX,
+    sourceY: props.sourceY,
+    targetX: props.targetX,
+    targetY: props.targetY,
+  }
+
+  const [edgeCenterX, edgeCenterY] = getEdgeCenter(randomDict);
+
+  return (
+    <>
+      <path id={props.id} style={props.style} className="react-flow__edge-path" d={edgePath} markerEnd={markerEnd} />
+
+      <text>
+        <textPath href={`#${props.id}`} style={{ fontSize: '12px' }} startOffset="50%" textAnchor="middle">
+          {props.data.text}
+        </textPath>
+      </text>
+
+      <foreignObject
+        width={foreignObjectSize}
+        height={foreignObjectSize}
+        x={props.targetX}
+        y={props.targetY}
+        className="edgebutton-foreignobject"
+        requiredExtensions="http://www.w3.org/1999/xhtml"
+      >
+        <div className="circle"></div>
+      </foreignObject>
+    </>
+  );
+}
