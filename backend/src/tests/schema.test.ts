727e5ce74c35ef30235fb7b5e937b8634b0e776e
Zhai Zirun <zz4319@ic.ac.uk>
2021-10-21 01:14:41 +0100

    feat: added jest for testing for backend

---
+++ backend/src/tests/schema.test.ts
@@ -0,0 +1,11 @@
+import SchemaController from "../controllers/schemaController";
+
+// run all tests with npx jest command
+
+test('basic', () => {    
+    expect(1 + 1).toBe(2);  
+});
+
+
+// TODO: add more tests
+// https://basarat.gitbook.io/typescript/intro-1/jest
