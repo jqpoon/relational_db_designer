eb7ebfb871409038d2c9aece986b7aa79fd2a5da
Vincent <kch19@ic.ac.uk>
2021-10-23 11:07:18 +0100

    added routes for demo

--- backend/src/routes/schema.ts
+++ backend/src/routes/schema.ts
@@ -1,30 +1,49 @@
 import { Router } from 'express';
+import { OK } from 'http-status-codes';
 import SchemaController from "../controllers/schemaController";
 
 const router = Router();
 
+router.get('/add_testing', async function(req, res, next) {
+    SchemaController.getInstance().testing({
+        id: 1,
+        name: 'Testing Entity One',
+    }, {
+        id: 2,
+        name: 'Testing Entity Two',
+    }, {
+        name: 'Testing Attribute',
+    }, {
+        name: 'Testing Relationship',
+        entities: [1, 2]
+    })
+
+    return res.status(OK).json({SUCCESS: true});
+})
+
 // Get Schema from Database
-router.get('/', function(req, res, next) {
+router.get('/entities', async function(req, res, next) {
     // TODO Julian request translation
     // console.log(req)
 
     // if ...
     // else ...
 
-    // SchemaController.getInstance().testing({
-    //     id: 1,
-    //     name: 'Testing Entity One',
-    // }, {
-    //     id: 2,
-    //     name: 'Testing Entity Two',
-    // }, {
-    //     name: 'Testing Attribute',
-    // }, {
-    //     name: 'Testing Relationship',
-    // })
+    const entities = SchemaController.getInstance().getAllEntities()
 
-    const result = SchemaController.getInstance().getAllRelationships()
+    entities.then(function (result) {
+        console.log(result)
+        return res.status(OK).send(result);
+    })
+});
 
+// Get Schema from Database
+router.get('/relationship', async function(req, res, next) {
+    const relationships = SchemaController.getInstance().getAllRelationships()
+
+    relationships.then(function (result) {
+        return res.status(OK).json({result});
+    })
 });
 
 export default router;
