d96d56d8686baf5cd5d7ec78f3a4a8a3e74a9fc9
jyw19 <jyw19@ic.ac.uk>
2021-11-15 15:07:02 +0000

    Change id to string in Disjoint interface

--- backend/src/models/disjoint.ts
+++ backend/src/models/disjoint.ts
@@ -3,7 +3,7 @@ import Position from "./position";
 // A disjoint is a category object that contains multiple entities.
 interface Disjoint {
     // An identifier given to every single object that exists in the ER model.
-    id: number;
+    id: string;
     text: String;
 
     // Variables used for generating the shape of the object on the client.
