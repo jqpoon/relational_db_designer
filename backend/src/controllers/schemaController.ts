b433a18991d9de4c1fe9c5260bd6731acfe37a76
Brandon <szh19@ic.ac.uk>
2021-12-01 18:02:42 +0000

    Debugged getAllEntities and getAllRelationships to send correct data

--- backend/src/controllers/schemaController.ts
+++ backend/src/controllers/schemaController.ts
@@ -53,38 +53,37 @@ class SchemaController {
             if (!entitiesHashMap.has(entity.properties.id)) {
                 entity.properties['pos'] = {
                     x: entity.properties.posX,
-                    y: entity.properties.posX,
+                    y: entity.properties.posY,
                 }
+								entity.properties['attributes'] = []
+								entity.properties['subsets'] = []
                 delete entity.properties.posX
                 delete entity.properties.posY
                 delete entity.properties.name
                 entitiesHashMap.set(entity.properties.id, entity.properties)
             }
         }
+				
 
         const entityWithAttributesResult: QueryResult = await DatabaseController.getInstance().getAllEntitiesWithAttributes()
 
         for (var e of entityWithAttributesResult.records) {
             for (let key in e.toObject()) {
                 var nodeID = e.toObject()[key]['nodeID']
-                var attributes = e.toObject()[key]['attributes']
+								if (entitiesHashMap.has(nodeID)) {
+									var attributes = e.toObject()[key]['attributes']
+									attributes.forEach((e: { properties: any; }) => {
+										e.properties['relativePos'] = {
+												x: e.properties.posX,
+												y: e.properties.posY,
+										}
+										delete e.properties.posX
+										delete e.properties.posY
+										delete e.properties.name
 
-                const entityAttributesList: Array<Attribute> = attributes.map(
-                    (e: { properties: any; }) => {
-                        e.properties['pos'] = {
-                            x: e.properties.posX,
-                            y: e.properties.posX,
-                        }
-                        delete e.properties.posX
-                        delete e.properties.posY
-                        delete e.properties.name
-                        return e.properties
-                    }
-                )
-
-                if (entitiesHashMap.get(nodeID) !== undefined) {
-                    entitiesHashMap.get(nodeID)!.attributes = entityAttributesList
-                }
+										entitiesHashMap.get(nodeID)!.attributes!.push(e.properties)
+									});
+								}
             }
         }
 
@@ -92,28 +91,24 @@ class SchemaController {
 
         for (var e of entityWithSubsetsResult.records) {
             for (let key in e.toObject()) {
-                var nodeID = e.toObject()[key]['nodeID']
-                var subsets = e.toObject()[key]['subsets']
+								var nodeID = e.toObject()[key]['nodeID']
+								if (entitiesHashMap.has(nodeID)) {
+									var subsets = e.toObject()[key]['subsets']
+									subsets.forEach((e: { properties: any; }) => {
+										e.properties['pos'] = {
+												x: e.properties.posX,
+												y: e.properties.posY,
+										}
+										delete e.properties.posX
+										delete e.properties.posY
+										delete e.properties.name
 
-                const subsetsList: Array<Entity> = subsets.map(
-                    (e: { properties: any; }) => {
-                        e.properties['pos'] = {
-                            x: e.properties.posX,
-                            y: e.properties.posX,
-                        }
-                        delete e.properties.posX
-                        delete e.properties.posY
-                        delete e.properties.name
-                        return e.properties
-                    }
-                )
-
-                if (entitiesHashMap.get(nodeID) !== undefined) {
-                    entitiesHashMap.get(nodeID)!.subsets = subsetsList
-                }
+										entitiesHashMap.get(nodeID)!.subsets!.push(e.properties)
+									});
+								}
             }
         }
-
+				
         return entitiesHashMap
     }
 
@@ -123,7 +118,7 @@ class SchemaController {
 
         // Use hashmap to update relationship O(1)
         const relationshipHashmap: Map<string, Relationship> = new Map()
-
+				
         for (var e of relationshipResult.records) {
             for (let key in e.toObject()) {
                 var relationship = e.toObject()[key]['relationship']
@@ -134,17 +129,18 @@ class SchemaController {
                 entities.map((e: {
                     entityID: string,
                     lhConstraint: {
-                        type: number
-                    }
+                        type: string
+                    }	
                 }) => {
-                    lhConstraint.set(e.entityID, LHConstraint[e.lhConstraint.type])
+                    lhConstraint.set(e.entityID, e.lhConstraint.type)
                 })
 
                 if (!relationshipHashmap.has(relationship.properties.id)) {
                     relationship.properties['pos'] = {
                         x: relationship.properties.posX,
-                        y: relationship.properties.posX,
+                        y: relationship.properties.posY,
                     }
+										relationship.properties['attributes'] = []
                     delete relationship.properties.posX
                     delete relationship.properties.posY
                     delete relationship.properties.name
@@ -156,34 +152,33 @@ class SchemaController {
                 }
             }
         }
+				
 
         const relationshipAttributeResult: QueryResult = await DatabaseController.getInstance().getAllRelationshipsWithAttributes()
 
         for (var e of relationshipAttributeResult.records) {
             for (let key in e.toObject()) {
                 var relationshipID = e.toObject()[key]['relationshipID']
-                var attributes = e.toObject()[key]['attributes']
-
-                const relationshipAttributeList: Array<Attribute> = attributes.map(
-                    (e: { properties: any; }) => {
-                        e.properties['pos'] = {
-                            x: e.properties.posX,
-                            y: e.properties.posX,
-                        }
-                        delete e.properties.posX
-                        delete e.properties.posY
-                        delete e.properties.name
-                        return e.properties
-                    }
-                )
-
-                if (relationshipHashmap.get(relationshipID) !== undefined) {
-                    relationshipHashmap.get(relationshipID)!.attributes = relationshipAttributeList
-                }
+								if (relationshipHashmap.has(relationshipID)) {
+									var attributes = e.toObject()[key]['attributes']
+									attributes.array.forEach(
+										(e: { properties: any; }) => {
+											e.properties['pos'] = {
+													x: e.properties.posX,
+													y: e.properties.posY,
+											}
+											delete e.properties.posX
+											delete e.properties.posY
+											delete e.properties.name
+											
+											relationshipHashmap.get(relationshipID)!.attributes!.push(e.properties)
+										}
+									)
+								}
             }
 
         }
-
+				
         return relationshipHashmap
     }
 
