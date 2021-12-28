1ab0c13385a51db84b42ca8338471c3e5ff2b289
Brandon <szh19@ic.ac.uk>
2021-12-28 19:56:06 +0000

    Further clean up of auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,14 +1,11 @@
-import {OK} from "http-status-codes";
 import {Router} from "express";
 import FirebaseController from "src/controllers/firebaseController";
 
 const router = Router();
 
 router.post('/signup', function (req, res) {
-    const modelAsJson = req.body;
-
-    const email: string = modelAsJson.email ?? undefined;
-    const password: string = modelAsJson.password ?? undefined;
+    const email: string = req.body.email ?? undefined;
+    const password: string = req.body.password ?? undefined;
 
     FirebaseController.getInstance().signUp(email, password)
 				.then((message: string) =>{
@@ -31,7 +28,7 @@ router.post('/login', function (req, res) {
 						res.status(200).send(message);
 				})
 				.catch((error) => {
-						res.status(400).send(error);
+						res.status(403).send(error);
 				});
 })
 
