5b2b1c219e30ad2eb8fbf187faf7de326740235c
Brandon <szh19@ic.ac.uk>
2021-12-28 20:33:02 +0000

    Rewrote auth controller with async await

--- backend/src/controllers/firebaseAuthController.ts
+++ backend/src/controllers/firebaseAuthController.ts
@@ -1,6 +1,5 @@
 import { FirebaseApp } from "firebase/app";
 import { 
-	Auth, 
 	UserCredential, 
 	getAuth, 
 	createUserWithEmailAndPassword, 
@@ -23,29 +22,26 @@ class FirebaseAuthController {
         return FirebaseAuthController.instance;
     }
 
-
-    public signUp(email: string, password: string): Promise<string> {
-			return createUserWithEmailAndPassword(this.auth, email, password).then(
-					() => {
-							return "User has been created";
-					})
-					.catch((error) => {
-							const errorMessage = error.message;
-							throw errorMessage;
-					});
+    public async signUp(email: string, password: string): Promise<string> {
+			try {
+					const user: UserCredential = 
+							await createUserWithEmailAndPassword(this.auth, email, password);
+					return user.user.uid;
+			} catch (error: any) {
+					const errorMessage = error.message;
+					throw errorMessage;
+			}
     }
 
-    public login(email: string, password: string): Promise<string> {
-			return signInWithEmailAndPassword(this.auth, email, password).then(
-					(userCredential: UserCredential) => {
-							// Signed in
-							const uid = userCredential.user.uid;
-							return uid;
-					})
-					.catch((error) => {
-							const errorMessage = error.message;
-							throw errorMessage;
-					});
+    public async login(email: string, password: string): Promise<string> {
+			try {
+					const user: UserCredential = 
+							await signInWithEmailAndPassword(this.auth, email, password);
+					return user.user.uid;
+			} catch (error: any) {
+					const errorMessage = error.message;
+					throw errorMessage;
+			}
     }
 
 }
