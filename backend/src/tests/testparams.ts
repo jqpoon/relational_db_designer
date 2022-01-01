ef78d9e4e574d4a3a9df0b812982c09e3ee31e5d
Brandon <szh19@ic.ac.uk>
2022-01-01 13:25:44 +0000

    Stashed test parameters into testparams.ts

---
+++ backend/src/tests/testparams.ts
@@ -0,0 +1,19 @@
+const email = "ONLY_FOR_TEST@gmail.com";
+const password = "123456";
+const badPassword = "123";
+const testUid = "FGQzwkGywpVzusiEDcFzHCZ50gk1";
+const otherUid = "cY67QQ0UneVYnhUpcZPnkjK1foF2";
+const badUid = "abc";
+const testErid = "USs55d7trSRHCoEumePy";
+const badErid = "abc123";
+
+export default {
+	email,
+	password,
+	badPassword,
+	testUid,
+	otherUid,
+	badUid,
+	testErid,
+	badErid
+}
