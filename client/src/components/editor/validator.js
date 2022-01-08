e4b605e0cd578f75031a67cec1f4566a76338871
jyw19 <jyw19@ic.ac.uk>
2022-01-08 17:22:33 +0000

    Fix syntax error in Validator, only translate if schema is valid, add comments

--- client/src/components/editor/validator.js
+++ client/src/components/editor/validator.js
@@ -1,26 +1,34 @@
 import { notificationHandler } from "./alerts/alert";
 
+// Validates a given schema in specific JSON format. See exportStateToObject in editor.js.
 export class Validator {
   constructor(schema) {
     this.schema = schema;
     this.entities = Object.values(schema.data.entities);
     this.relationships = Object.values(schema.data.relationships);
     this.edges = Object.values(schema.data.edges);
+
+    // Whether the current schema is valid.
     this.valid = true;
+    // Keeps track of all errors until they are dumped when error message is displayed.
     this.errors = [];
   }
 
+  // Add an error to the error stack and mark the schema as invalid.
   flagInvalid(error) {
     this.valid = false;
     this.errors.push(error);
   }
 
+  // Generate some display name for an entity or relationship, even if the text field is empty.
+  // Used in generating error messages.
   findNodeName(node) {
     return (node.text === "") ? node.id : node.text;
   }
 
   // Entities.
 
+  // Whether an entity has been given a name.
   checkForEntityText(e) {
     if (e.text === "") this.flagInvalid("Entity \"" + e.id + "\" has no name.");
     Object.values(e.attributes).forEach(a => {
@@ -31,6 +39,7 @@ export class Validator {
     });
   }
 
+  // Whether an entity has a single primary key (if applicable).
   checkForEntityPrimaryKey(e) {
     const attributes = Object.values(e.attributes);
 
@@ -51,12 +60,14 @@ export class Validator {
 
   // Relationships.
 
+  // Whether a relationship has been given a name.
   checkForRelationshipText(r) {
     if (r.text === "") this.flagInvalid("Relationship \"" + r.id + "\" has no name.");
   }
 
   // Public methods.
 
+  // Runs each test to see if schema is valid or not.
   validate() {
     this.entities.forEach(e => {
       this.checkForEntityText(e);
@@ -68,6 +79,7 @@ export class Validator {
     });
   }
 
+  // Validate, then generate an error pop-up for the client-side.
   validateAndAlert() {
     this.validate();
 
