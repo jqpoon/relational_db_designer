274821ab73a5ac2781d56d24a3e91f165ac7b112
wyt <tee.weiyi22@gmail.com>
2022-01-10 15:15:47 +0000

    Bug fix: incorrectly adding hierarchy edges

--- client/src/components/editor/elements/hierarchyEdges/hierarchyEdge.js
+++ client/src/components/editor/elements/hierarchyEdges/hierarchyEdge.js
@@ -18,68 +18,77 @@ import { getId } from "../../utilities/idGenerator";
 // 2. CRUD for hierarchy edge
 // **************************
 
-export const createHierarchyEdge = (child, parent, generalisation) => {
-	return {
-		id: getId(types.EDGE.HIERARCHY, parent.id, child.id),
-		type: types.EDGE.HIERARCHY,
-		child: child.id,
-		parent: parent.id,
-		generalisation: generalisation,
-	};
+export const createHierarchyEdge = (child, parent) => {
+  let edge = {
+    id: getId(types.EDGE.HIERARCHY, parent.id, child.id),
+    type: types.EDGE.HIERARCHY,
+    child: child.id,
+  };
+  if (parent.type === types.GENERALISATION) {
+    edge.parent = parent.parent.id;
+    edge.generalisation = parent.id;
+  } else {
+    edge.parent = parent.id;
+  }
+  return edge;
 };
 
 export const getHierarchyEdge = ({ edges }, id) => {
-	return edges[id] ? { ...edges[id] } : null;
+  return edges[id] ? { ...edges[id] } : null;
 };
 
 export const updateHierarchyEdge = ({ elements, setElements }, edge) => {
-	let oldEntry = elements.edges[edge.id];
-	let data = { node: null, edges: [oldEntry ? oldEntry : edge] };
-	data = JSON.parse(JSON.stringify(data));
+  let oldEntry = elements.edges[edge.id];
+  let data = { node: null, edges: [oldEntry ? oldEntry : edge] };
+  data = JSON.parse(JSON.stringify(data));
 
-	setElements((prev) => {
-		let newElements = { ...prev };
-		const { entities, edges } = newElements;
-		edges[edge.id] = edge;
-		if (!oldEntry) {
-			// Newly added edge, update source and target
-			entities[edge.child].edges[edge.id] = { type: types.EDGE.HIERARCHY };
-			if (edge.generalisation) {
-				entities[edge.parent].generalisations[edge.generalisation].edges[edge.id] = {
-					type: types.EDGE.HIERARCHY,
-				};
-			} else {
-				entities[edge.parent].edges[edge.id] = {
-					type: types.EDGE.HIERARCHY,
-				};
-			}
-		}
-		return newElements;
-	});
+  setElements((prev) => {
+    let newElements = { ...prev };
+    const { entities, edges } = newElements;
+    edges[edge.id] = edge;
+    if (!oldEntry) {
+      // Newly added edge, update source and target
+      entities[edge.child].edges[edge.id] = { type: types.EDGE.HIERARCHY };
+      if (edge.generalisation) {
+        entities[edge.parent].generalisations[edge.generalisation].edges[
+          edge.id
+        ] = {
+          type: types.EDGE.HIERARCHY,
+        };
+      } else {
+        entities[edge.parent].edges[edge.id] = {
+          type: types.EDGE.HIERARCHY,
+        };
+      }
+    }
+    return newElements;
+  });
 
-	return data;
+  return data;
 };
 
 export const deleteHierarchyEdge = ({ elements, setElements }, edge) => {
-	let data = { node: null, edges: [edge] };
-	data = JSON.parse(JSON.stringify(data));
+  let data = { node: null, edges: [edge] };
+  data = JSON.parse(JSON.stringify(data));
 
-	setElements((prev) => {
-		let newElements = { ...prev };
-		const { entities, edges } = newElements;
-		// Delete references to edge in nodes
-		delete entities[edge.child].edges[edge.id];
-		if (edge.generalisation) {
-			delete entities[edge.parent].generalisations[edge.generalisation].edges[edge.id];
-		} else {
-			delete entities[edge.parent].edges[edge.id];
-		}
-		// Delete edge
-		delete edges[edge.id];
-		return newElements;
-	});
+  setElements((prev) => {
+    let newElements = { ...prev };
+    const { entities, edges } = newElements;
+    // Delete references to edge in nodes
+    delete entities[edge.child].edges[edge.id];
+    if (edge.generalisation) {
+      delete entities[edge.parent].generalisations[edge.generalisation].edges[
+        edge.id
+      ];
+    } else {
+      delete entities[edge.parent].edges[edge.id];
+    }
+    // Delete edge
+    delete edges[edge.id];
+    return newElements;
+  });
 
-	return data;
+  return data;
 };
 
 // ***************************
@@ -87,16 +96,16 @@ export const deleteHierarchyEdge = ({ elements, setElements }, edge) => {
 // ***************************
 
 export function HierarchyEdge({ parent, child, generalisation, scale }) {
-	return (
-		<Xarrow
-			start={child.toString()}
-			end={(generalisation ? generalisation : parent).toString()}
-			showHead={!generalisation}
-			curveness={0}
-			endAnchor={generalisation ? "middle" : "auto"}
-			startAnchor="middle"
-			strokeWidth={4 * scale}
-			zIndex={-1}
-		/>
-	);
+  return (
+    <Xarrow
+      start={child.toString()}
+      end={(generalisation ? generalisation : parent).toString()}
+      showHead={!generalisation}
+      curveness={0}
+      endAnchor={generalisation ? "middle" : "auto"}
+      startAnchor="middle"
+      strokeWidth={4 * scale}
+      zIndex={-1}
+    />
+  );
 }
